b9c6f7150fbdb9afdacb0b51cfac613430fdc8a4
Mountain720 <70184721+mountain720@users.noreply.github.com>
2021-02-25 12:22:32 -0700

    Update PopupPage.js

--- src/pages/PopupPage.js
+++ src/pages/PopupPage.js
@@ -244,7 +244,7 @@ function ApproveConnectionForm({ origin, onApprove }) {
     <Card>
       <CardContent>
         <Typography variant="h6" component="h1" gutterBottom>
-          Allow this site to access your Solana account?
+          Allow this site to access your SafeCoin account?
         </Typography>
         <div className={classes.connection}>
           <Typography>{origin}</Typography>
