b1a9f2f9b54a27826100801fe2786665027332e3
Mountain720 <70184721+mountain720@users.noreply.github.com>
2021-02-28 13:57:18 -0700

    Update AddAccountDialog.js

--- src/components/AddAccountDialog.js
+++ src/components/AddAccountDialog.js
@@ -7,7 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormGroup from '@material-ui/core/FormGroup';
 import Switch from '@material-ui/core/Switch';
-import { Account } from '@solana/web3.js';
+import { Account } from '@safecoin/web3.js';
 import DialogForm from './DialogForm';
 
 export default function AddAccountDialog({ open, onAdd, onClose }) {
