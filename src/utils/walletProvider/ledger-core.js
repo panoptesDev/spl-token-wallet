c7db9727d6ce6b5f3eb121e83024c9e3959f2cb0
Mountain720 <70184721+mountain720@users.noreply.github.com>
2021-02-28 22:58:24 -0700

    Update ledger-core.js

--- src/utils/walletProvider/ledger-core.js
+++ src/utils/walletProvider/ledger-core.js
@@ -70,7 +70,7 @@ export function solana_derivation_path(account, change) {
   var offset = 0;
   offset = derivation_path.writeUInt8(length, offset);
   offset = derivation_path.writeUInt32BE(_harden(44), offset); // Using BIP44
-  offset = derivation_path.writeUInt32BE(_harden(501), offset); // Solana's BIP44 path
+  offset = derivation_path.writeUInt32BE(_harden(19165), offset); // Solana's BIP44 path
   offset = derivation_path.writeUInt32BE(_harden(useAccount), offset);
   derivation_path.writeUInt32BE(_harden(useChange), offset);
 
